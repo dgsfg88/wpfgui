b600bbe58fb9e71cce671c4957a8533b0c7b9f13
Andrea Ianni <andreaianni@hotmail.com>
2025-05-02 21:29:47 +0200

    #enhancement moved the style of scrollviewer in a dictionary to make it generic

---
+++ source/Styles/Styles.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpfgui.Styles
+{
+    public partial class Styles : ResourceDictionary
+	{
+		public Styles()
+		{
+			InitializeComponent();
+		}
+	}
+}
